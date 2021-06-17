0fd8598bc91ece84d94afbb4227df2745a0fcd0c
Linus Hoppe <hoppelinus@web.de>
2021-06-17 21:41:45 +0200

    test

--- main/main.ino
+++ main/main.ino
@@ -1,46 +1,141 @@
-volatile unsigned int temp, counter = 0; //This variable will increase or decrease depending on the rotation of encoder
+// Prozesse
+TaskHandle_t Task1;
+TaskHandle_t Task2;
 
-int p4 = 5; 
-int p5 = 4;
-    
+
+
+//-------------------------------------------
+// Motor A
+int motor1Pin1 = 27; 
+int motor1Pin2 = 26; 
+int enable1Pin = 14; 
+
+// Setting PWM properties
+const int freq = 30000;
+const int pwmChannel = 0;
+const int resolution = 8;
+int dutyCycle = 200;
+//-------------------------------------------
+
+// Button-----------------------------------
+#define stsp_button 16 
+
 void setup() {
-  Serial.begin (9600);
-  Serial.print("Hallo");
-  pinMode(p4, INPUT_PULLUP); // internal pullup input pin 2 
+  //Motor------------------------------------
+  // sets the pins as outputs:
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(enable1Pin, OUTPUT);
   
-  pinMode(p5, INPUT_PULLUP); // internal pullup input pin 3
-//Setting up interrupt
-  //A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 2 on moust Arduino.
-  attachInterrupt(p4, ai0, RISING);
-   
-  //B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is DigitalPin nr 3 on moust Arduino.
-  attachInterrupt(p5, ai1, RISING);
+  // configure LED PWM functionalitites
+  ledcSetup(pwmChannel, freq, resolution);
+  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(enable1Pin, pwmChannel);
+  //------------------------------------------
+
+  //Prozesse----------------------------------
+  //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+  xTaskCreatePinnedToCore(
+                    Task1code,   /* Task function. */
+                    "Task1",     /* name of task. */
+                    10000,       /* Stack size of task */
+                    NULL,        /* parameter of the task */
+                    1,           /* priority of the task */
+                    &Task1,      /* Task handle to keep track of created task */
+                    0);          /* pin task to core 0 */                  
+  delay(500); 
+
+  //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
+  xTaskCreatePinnedToCore(
+                    Task2code,   /* Task function. */
+                    "Task2",     /* name of task. */
+                    10000,       /* Stack size of task */
+                    NULL,        /* parameter of the task */
+                    1,           /* priority of the task */
+                    &Task2,      /* Task handle to keep track of created task */
+                    1);          /* pin task to core 1 */
+    delay(500); 
+  //-------------------------------------------------
+
+  //Button------------------------------------------
+  pinMode(stsp_button, INPUT);
+  
+  
+
+  Serial.begin(115200);
+
+}
+
+// Task 1
+void Task1code( void * pvParameters ){
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  // it like loop, but just for that task
+  for(;;){
+//      dutyCycle = 200;
+//      start_DC_rough();
+//      delay(5000);
+//      stop_DC_rough();
+//      delay(5000);
+        if (digitalRead(stsp_button) == LOW){
+          start_DC();
+        }
+  } 
+}
+
+
+// Task 2
+// @DC Motor 
+void Task2code( void * pvParameters ){
+  Serial.print("Task2 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  // it like loop, but just for that task
+  for(;;){
+
+  
   }
-   
-  void loop() {
-  // Send the value of counter
-  if( counter != temp ){
-  Serial.println (counter);
-  temp = counter;
+}
+
+void stop_DC_rough(){
+  Serial.println("Motor stopped");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+  
+}
+
+void start_DC_rough(){
+  //  Move the DC motor forward at maximum speed
+  Serial.println("Moving Forward");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH); 
+}
+
+void start_DC(){
+  // Move DC motor forward with increasing speed
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+  while (dutyCycle <= 255){
+    ledcWrite(pwmChannel, dutyCycle);   
+    Serial.print("Forward with duty cycle: ");
+    Serial.println(dutyCycle);
+    dutyCycle = dutyCycle + 5;
+    delay(500);
   }
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+  dutyCycle = 255;
+  while(digitalRead(stsp_button) == HIGH){
+    ledcWrite(pwmChannel, dutyCycle); 
+    delay(500);
   }
-   
-  void ai0() {
-  // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
-  // Check pin 3 to determine the direction
-  if(digitalRead(p5)==LOW) {
-  counter++;
-  }else{
-  counter--;
-  }
-  }
-   
-  void ai1() {
-  // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
-  // Check with pin 2 to determine the direction
-  if(digitalRead(p4)==LOW) {
-  counter--;
-  }else{
-  counter++;
-  }
-  }
+  stop_DC_rough();
+  delay(5000);
+}
+
+
+void loop() {
+
+}
