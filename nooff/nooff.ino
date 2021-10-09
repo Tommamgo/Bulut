bb1832ac33c950cd32f1df9d75629c0ee613bd04
the <hoppelinus@web.de>
2021-10-09 13:38:34 +0200

    The new Display test

--- nooff/nooff.ino
+++ nooff/nooff.ino
@@ -3,31 +3,72 @@ const uint8_t c_in  = 18;
 // will give the input value the current
 const uint8_t c_out = 19; 
 
+int credit_conut50 = 0; 
+
+bool on_off;  
+
+
+void IRAM_ATTR coinINPUT(){
+  //Serial.print("We got an new Coin");
+  //credit_conut50++;
+  on_off = true; 
+  Serial.println("Hallo"); 
+  //Serial.println("Credit balance: " + String(credit, 2));
+}
+
+void power_off(){
+  if(on_off){
+    on_off = false; 
+  }
+}
+
+
+
 
 
 void setup(){
-  // init bought 
+  // Serial
+  Serial.begin(9600);
+  
+  // init bought
   pinMode(c_in, INPUT); 
   pinMode(c_out, OUTPUT); 
+  
+  attachInterrupt(digitalPinToInterrupt(c_in), coinINPUT, RISING); // fot interrupt_Mode
+
 
   
 }
 
 void loop(){
+ 
   
-  c_state(c_in, c_out); 
+  //c_state(c_in, c_out); 
 
-  delay(10);
+  Serial.println(on_off); 
+
+  if(on_off){
+    digitalWrite(c_out, HIGH); 
+  }else{
+    digitalWrite(c_out, LOW); 
+  }
+  power_off();
+  delay(1000);
+   
   
 }
 
 
 
+
+
 bool c_state(uint8_t in, uint8_t out){
     if(digitalRead(in)){
+      Serial.println("Hallo es liegt nicht an"); 
       digitalWrite(out, LOW); 
       return true; 
     }else{
+      Serial.println("Hallo es liegt etwas an"); 
       digitalWrite(out, HIGH);
       return false;
     }
